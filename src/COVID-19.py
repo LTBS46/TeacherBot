86139ad2870114b14dbf8f7172419784604ecb2c
Baptiste <40568322+LTBS46@users.noreply.github.com>
2020-03-13 21:31:40 +0100

    Update COVID-19.py

--- src/COVID-19.py
+++ src/COVID-19.py
@@ -1,5 +1,35 @@
-import urllib as _url
+import urllib as _url, json as _js
 
+def get_string_from_url(url):
+    str = ''
+    try:
+        f = _url.urlopen(url)
+        str = f.read()
+    except:
+        print("cannot access : {0}".format(url))
+    finally:
+        f.close()
+    return str
+
+def get_dimension():
+    str = get_string_from_url("https://ghoapi.azureedge.net/api/Dimension")
+    obj = _js.loads(str)
+    code_list = []
+    title_list = []
+    for c in obj[u'value']:
+        code_list.append(i[u'Code'])
+        title_list.append(i[u'Title'])
+    return (code_list, title_list)
+
+def get_indicator():
+    str = get_string_from_url('https://ghoapi.azureedge.net/api/Indicator')
+    obj = _js.loads(str)
+    name_list = []
+    code_list = []
+    for c in obj[u'value']:
+        code_list.append(i[u'IndicatorCode'])
+        name_list.append(i[u'IndicatorName'])
+    return (code_list, name_list)
 
 
 del _url
